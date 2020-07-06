7b5b1542a0cdf0bea45d018c4c1aee1be71fad38
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-05 22:50:41 -0500

    setting answer routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-
+  res.render('index')
 })
 
 router.get('/signup', (req, res, next) => {
--- src/server.js
+++ src/server.js
@@ -1,14 +1,23 @@
-const express = require("express");
+const express = require('express')
+const engine = require('ejs-mate')
+const path = require('path')
+const morgan = require('morgan')
 
 // initializations
-const app = express();
+const app = express()
 
 // settings
-app.set("port", process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'))
+app.engine('ejs', engine)
+app.set('view engine', 'ejs')
+app.set('port', process.env.PORT || 3000)
+
+// middlewares
+app.use(morgan('dev'));
 
 // routes
-app.use("/", require("./routes/index"));
+app.use('/', require('./routes/index'))
 
-app.listen(app.get("port"), () => {
-  console.log("server on port", app.get("port"));
-});
+app.listen(app.get('port'), () => {
+  console.log('server on port', app.get('port'))
+})
