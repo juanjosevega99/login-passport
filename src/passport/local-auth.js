5e2b6753cd55058d1dc4c6d438141d818fc43bcb
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-05 23:43:14 -0500

    passport serializer and deserializer

--- src/passport/local-auth.js
+++ src/passport/local-auth.js
@@ -3,10 +3,28 @@ const LocalStrategy = require('passport-local').Strategy
 
 const User = require('../models/user')
 
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id)
+  done(null, user)
+})
+
 passport.use('local-signup', new LocalStrategy({
   email: 'email',
   password: 'password',
   passReqToCallback: true
 }, async (req, email, password, done) => {
   const user = await User.findOne({ 'email': email })
+  if (user) {
+    return done(null, false, req.flash('signupMessage', 'The Email is already token'))
+  } else {
+    const newUser = new User()
+    newUser.email = email
+    newUser.passport = newUser.encryptPassword(password)
+    await newUser.save()
+    done(null, newUser)
+  }
 }))
