e197afb6a4383c19fe3fdc6522c02af028008b88
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-05 23:06:28 -0500

    base signup

--- src/passport/local-auth.js
+++ src/passport/local-auth.js
@@ -2,3 +2,11 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 
 const User = require('../models/user')
+
+passport.use('local-signup', new LocalStrategy({
+  email: 'email',
+  password: 'password',
+  passReqToCallback: true
+}, async (req, email, password, done) => {
+  const user = await User.findOne({ 'email': email })
+}))
